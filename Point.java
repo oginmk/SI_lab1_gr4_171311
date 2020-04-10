18918bd99eeb6288b7835053e8b7d4b51887027f
Ognen Anastasov <ognen_a@hotmail.com>
2020-04-10 23:29:00 +0200

    points class finished

--- Point.java
+++ Point.java
@@ -6,14 +6,56 @@ class Point {
 
 	//TODO constructor
 
-	//TODO setters and getters
+	public Point(String id, double x, double y, String boja) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
+//TODO setters and getters
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		setX(getX()+xDirection);
+		setY(getY()+yDirection);
+
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println(getX() + getY())
 	}
 
 
